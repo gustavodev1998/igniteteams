a8a886a98969d6618f66771d344ebe00b7fd195a
Campos <gustavo-henrique.campos@capgemini.com>
2024-04-16 15:04:22 +0100

    add lista de grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 import * as S from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera da Rocket",
+    "Amigos",
+  ]);
+
   return (
     <S.Container>
       <Header />
+
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </S.Container>
   );
 }
